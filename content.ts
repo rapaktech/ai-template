5f3ccb3166ab364132795628f6de71f9a27aaaf6
Jim Ezesinachi <ezesinachijim@gmail.com>
2023-02-27 17:32:35 +0100

    added psychic text

--- content.ts
+++ content.ts
@@ -1,17 +1,15 @@
 // IMPORTANT: You can change your prompt here
 export const BASE_PROMPT_PREFIX = `
-  I want you to act as a comedian. 
-  I will provide you with a topic and you will use your wit, creativity and skills to create a joke around this topic. 
-  Make it a very dry joke, like a dad joke. 
+  I want you to act as a psychic. 
+  I will provide you with a topic and you will use your wit, creativity and skills to create a prediction around this topic.  
   My topic is: `;
 
 // IMPORTANT: You can change the copy text here
-export const SUB_TITLE = "ALWAYS BE HILARIOUS";
-export const TITLE = "GPT-Joker";
-export const TEXT = `Get your daily dose of laughter with our AI-powered joke generator.
-From witty one-liners to hilarious puns, our algorithm has got you
-covered`;
+export const SUB_TITLE = "See the future";
+export const TITLE = "GPT-Psychic";
+export const TEXT = `Know the future with our AI-powered psychic predictor.
+Ask this psychic to predict about anything!`;
 
 // IMPORTANT: You can change the button labels here
-export const INPUT_LABEL = "Enter a topic for a joke";
-export const BUTTON_LABEL = "Make me laugh ✨";
+export const INPUT_LABEL = "Enter a topic for our psychic to predict about";
+export const BUTTON_LABEL = "Tell me the future✨";
